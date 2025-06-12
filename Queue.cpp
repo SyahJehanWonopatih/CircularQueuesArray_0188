efac107dc9b2e25439dd07320419735665171110
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-06-12 10:30:53 +0700

    menambahkan fungsi insert untuk memasukan number

--- Queue.cpp
+++ Queue.cpp
@@ -12,5 +12,14 @@ class queues {
             front = -1;
             rear = -1;
         }
-       
+        void insert() {
+            int num;
+            cout << "Enter a number: ";
+            cin >> num;
+            cout << endl;
+
+     
+        }
+
+    
 };
